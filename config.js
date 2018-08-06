f4d77000f1df1a914e35a0d51b65a70f0b49f0f7
UltraNote Support <support@ultranote.org>
2018-08-06 16:53:20 +0000

    config.js update

--- config.js
+++ config.js
@@ -6,9 +6,9 @@ var symbol = 'XUN';
 var refreshDelay = 30000;
 // pools stats by MainCoins
 var networkStat = {
-	"xun": [
-		["Alpha", "http://alpha.ultranote.org:8117/stats"],
-                ["Beta", "http://beta.ultranote.org:8118/stats"],
-		["Delta", "http://delta.ultranote.org:8117/stats"]
+        "xun": [
+                ["Alpha.ultranote.org", "http://alpha.ultranote.org:8117/stats"],
+                ["Beta.ultranote.org", "http://beta.ultranote.org:8117/stats"],
+                ["Delta.ultranote.org", "http://delta.ultranote.org:8117/stats"]
     ]
 };
